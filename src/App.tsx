0e203337bed105df897ae79a3674956d94324997
hamaddo <hamaddo@yandex.ru>
2023-03-08 17:55:41 +0700

    feat: define base url by env feat: integrate sign out

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { useLogto } from '@logto/react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://admin.test.bmw-nsk.ru' : 'http://localhost:5173';
 export const RootApp = () => {
   const [count, setCount] = useState(0);
 
-  const { signIn, isAuthenticated } = useLogto();
+  const { signIn, isAuthenticated, signOut } = useLogto();
 
   if (!isAuthenticated) {
-    signIn('http://localhost:5173/callback');
+    signIn(`${BASE_URL}/callback`);
   }
 
   return (
@@ -27,6 +28,7 @@ export const RootApp = () => {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => signOut(BASE_URL)}>sign out</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
